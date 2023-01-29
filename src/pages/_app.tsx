61132b47b35f988d9d2b0a518a63b10b02898521
Milena Brum <milena.brum@nowports.com>
2023-01-29 12:19:26 -0300

    added chakra theme to improve app's look&feel

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
+import { theme } from '../chakra/theme';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       {/* eslint-disable-next-line react/jsx-props-no-spreading */}
       <Component {...pageProps} />
     </ChakraProvider>
