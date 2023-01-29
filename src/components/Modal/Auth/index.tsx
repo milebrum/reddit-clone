288040ce321c61a3064548cbb130daeaee83949a
Milena Brum <milena.brum@nowports.com>
2023-01-29 18:44:50 -0300

    create login and sign up modals for user auth

--- src/components/Modal/Auth/index.tsx
+++ src/components/Modal/Auth/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {
+  Flex,
   Modal,
   ModalBody,
   ModalCloseButton,
@@ -9,6 +10,7 @@ import {
 } from '@chakra-ui/react';
 import { useRecoilState } from 'recoil';
 import { authModalState } from '../../../atoms/authModalAtom';
+import AuthInputs from './AuthInputs';
 
 const AuthModal: React.FC = () => {
   const [modalState, setModalState] = useRecoilState(authModalState);
@@ -23,9 +25,25 @@ const AuthModal: React.FC = () => {
     <Modal isOpen={modalState.open} onClose={handleClose}>
       <ModalOverlay />
       <ModalContent>
-        <ModalHeader>Modal Title</ModalHeader>
+        <ModalHeader textAlign="center">
+          {modalState.view === 'login' && 'Login'}
+          {modalState.view === 'signup' && 'Sign Up'}
+          {modalState.view === 'resetPassword' && 'Reset Password'}
+        </ModalHeader>
         <ModalCloseButton />
-        <ModalBody>Here is the modal body</ModalBody>
+        <ModalBody
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          justifyContent="center"
+          pb={6}
+        >
+          <Flex direction="column" align="center" justify="center" width="70%">
+            {/* <OAuthButtons /> */}
+            <AuthInputs />
+            {/* <ResetPassword /> */}
+          </Flex>
+        </ModalBody>
       </ModalContent>
     </Modal>
   );
