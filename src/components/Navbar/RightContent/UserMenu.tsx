0e73bcd3bb3d33570d2d445392813a85681a0fc7
Milena Brum <milena.brum@nowports.com>
2023-01-30 18:04:37 -0300

    add communities dropdown in navbar

--- src/components/Navbar/RightContent/UserMenu.tsx
+++ src/components/Navbar/RightContent/UserMenu.tsx
@@ -94,19 +94,17 @@ const UserMenu: React.FC<UserMenuProps> = ({ user }) => {
             </MenuItem>
           </>
         ) : (
-          <>
-            <MenuItem
-              fontSize="10pt"
-              fontWeight={700}
-              _hover={{ bg: 'blue.500', color: 'white' }}
-              onClick={() => setAuthModalState({ open: true, view: 'login' })}
-            >
-              <Flex align="center">
-                <Icon fontSize={20} mr={2} as={MdOutlineLogin} />
-                Log In / Sign Up
-              </Flex>
-            </MenuItem>
-          </>
+          <MenuItem
+            fontSize="10pt"
+            fontWeight={700}
+            _hover={{ bg: 'blue.500', color: 'white' }}
+            onClick={() => setAuthModalState({ open: true, view: 'login' })}
+          >
+            <Flex align="center">
+              <Icon fontSize={20} mr={2} as={MdOutlineLogin} />
+              Log In / Sign Up
+            </Flex>
+          </MenuItem>
         )}
       </MenuList>
     </Menu>
